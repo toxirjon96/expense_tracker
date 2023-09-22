1db929cf30c1b269f464943ed0d010f9ac406016
toxirjon96 <tatu.toxirjon@gmail.com>
2023-09-22 10:05:09 +0500

    app modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
 import 'package:expense_tracker/src/expense_tracker_library.dart';
 
 void main() {
+  final kColorSchema = ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(
+      255,
+      96,
+      59,
+      181,
+    ),
+  );
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Expense tracker",
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorSchema,
+          appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: kColorSchema.onPrimaryContainer,
+            foregroundColor: kColorSchema.primaryContainer,
+          )),
       home: const Expenses(),
     ),
   );
