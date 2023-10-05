e66f39abfcb27775925e358f7804c588c1de8d23
toxirjon96 <tatu.toxirjon@gmail.com>
2023-10-05 21:34:25 +0500

    app modified

--- lib/src/ui/expenses.dart
+++ lib/src/ui/expenses.dart
@@ -29,8 +29,34 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text("Expense deleted"),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(
+                expenseIndex,
+                expense,
+              );
+            });
+          },
+        ),
+      ),
+    );
+  }
+
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(
         onAddExpense: _addNewExpense,
@@ -40,6 +66,15 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text("No expense found! Try to add new one!"),
+    );
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expensesList: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text("Expense tracker"),
@@ -53,7 +88,9 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           const Text("The Chart ..."),
-          Expanded(child: ExpensesList(expensesList: _registeredExpenses))
+          Expanded(
+            child: mainContent,
+          ),
         ],
       ),
     );
--- lib/src/ui/expenses_list.dart
+++ lib/src/ui/expenses_list.dart
@@ -1,19 +1,27 @@
 import 'package:expense_tracker/src/expense_tracker_library.dart';
 
 class ExpensesList extends StatelessWidget {
-  final List<Expense> expensesList;
-
   const ExpensesList({
     super.key,
     required this.expensesList,
+    required this.onRemoveExpense,
   });
+
+  final List<Expense> expensesList;
+  final Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expensesList.length,
-      itemBuilder: (ctx, index) => ExpensesItem(
-        expense: expensesList[index],
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expensesList[index]),
+        onDismissed: (direction) =>{
+          onRemoveExpense(expensesList[index]),
+        },
+        child: ExpensesItem(
+          expense: expensesList[index],
+        ),
       ),
     );
   }
